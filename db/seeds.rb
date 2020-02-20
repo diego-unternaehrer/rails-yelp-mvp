bc5ecb8cbfa68dde0821307e298536c3370b3188
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2020-02-20 08:40:32 -0300

    Added controllers and views for the restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.create(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: Faker::Restaurant.type
+  )
+end
+
+puts 'Finished!'
